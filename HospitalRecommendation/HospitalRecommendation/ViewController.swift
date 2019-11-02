df2c909d68b3b95e73181c61948abe8968e255bc
Szmbbq <github4szmbbq>
2019-11-02 10:52:34 -0400

    create blank projec

---
+++ HospitalRecommendation/HospitalRecommendation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HospitalRecommendation
+//
+//  Created by xxx on 2019/11/2.
+//  Copyright © 2019 Szmbbq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
